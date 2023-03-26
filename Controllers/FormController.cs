0f098b73e754e02280034d21ef0b6662bd455f86
PlichtaM <113388036+PlichtaM@users.noreply.github.com>
2023-03-26 22:47:01 +0200

    Add files via upload

---
+++ Controllers/FormController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace zadanie.Controllers
+{
+    public class FormController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string field1, string field2, string field3)
+        {
+            ViewBag.Field1 = field1;
+            ViewBag.Field2 = field2;
+            ViewBag.Field3 = field3;
+            return View("Confirmation");
+        }
+    }
+}
---
+++ Controllers/ImageController.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace zadanie.Controllers
+{
+    public class ImageController : Controller
+    {
+        public IActionResult Index(string name)
+        {
+            var imagePath = Path.Combine("wwwroot", "images", name);
+            if (System.IO.File.Exists(imagePath))
+            {
+                var image = System.IO.File.ReadAllBytes(imagePath);
+                return File(image, "image/jpeg");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
